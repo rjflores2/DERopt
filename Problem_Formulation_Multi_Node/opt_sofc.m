6c7f68c8ce968a78b0dbbed15bb9f0dd7f9f5e26
Pegah <33164080+pegmot@users.noreply.github.com>
2022-05-09 23:54:52 -0700

    # of operating SOFC second method
    var_sofc.sofc_opstate = zeros(1,K). The optimized solution always gives similar numbers of SOFC and the operating ones. I guess I am missing something. it makes sense if it is defined as var_sofc.sofc_opstate = zeros(T,K), but there is matrix dimension mismatch error

--- Problem_Formulation_Multi_Node/opt_sofc.m
+++ Problem_Formulation_Multi_Node/opt_sofc.m
@@ -3,7 +3,7 @@ if ~isempty(sofc_v)
     for i=1:K
          
             Constraints = [Constraints,
-                (var_sofc.sofc_opstate(:,i) <= var_sofc.sofc_number):'Limit operating SOFCs to purchased SOFC'  % # operating SOFC limit
+                (var_sofc.sofc_opstate<= var_sofc.sofc_number):'Limit operating SOFCs to purchased SOFC'  % # operating SOFC limit
                 (var_sofc.sofc_elec(:,i) <= sofc_v(5)*var_sofc.sofc_opstate):'Max elec limited by operating SOFCs'    % electricity generation
                 ((sofc_v(4)*var_sofc.sofc_elec(:,i))/sofc_v(3) == var_sofc.sofc_heat(:,i)):'SOFC Heat Generation' %Cogenerated heat by SOFC
                 (var_sofc.sofc_wh(:,i) <= var_sofc.sofc_heat(:,i)):'Max heat limited by SOFC capacity'  %the heat used for water heating cannot be more than available heat from CHP_SOFC
