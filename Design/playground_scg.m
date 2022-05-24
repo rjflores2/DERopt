af184125e746a9210301e08263747426af237f0c
Pegah <33164080+pegmot@users.noreply.github.com>
2022-05-24 12:07:27 -0700

    DUMMY NG COST deleted
     DUMMY NG COST deleted.
    urg_adder = [0.03]; selected for 2035

--- Design/playground_scg.m
+++ Design/playground_scg.m
@@ -71,9 +71,9 @@ nem_rate = 3.0;
 %%% Island operation (opt_nem.m)
 island = 0;
 
-%%%Utility Cost Increase ($/kWh) $0.03/kWh for 60% RPS, $0.05/kWh for 100%
+%%%Utility Cost Increase ($/kWh) 2035: $0.03/kWh for 60% RPS, 2045: $0.05/kWh for 100%
 %%%RPS
-urg_adder = [0];
+urg_adder = [0.03]; %Utility retain generation urg_adder
 
 %% Utility Gas Properties
 h2_mix = 0.2; %Gas mixture assumption (%/vol)
@@ -122,7 +122,7 @@ addpath(genpath('H:\_Research_\CEC_OVMG\Rates'))
 %% Loading building demand - BEopt Data Ref Format
 
 % dt = xlsread('UO_Example.xlsx');
-dt = readtable('CZ08_2020_Ref.csv');
+dt = readtable('CZ06_2020_Ref.csv');
 
 %%%All energy is in kWh
 time = (dt.HoursSince00_00Jan1 - 0.5)./24;
@@ -157,7 +157,7 @@ apartment_types = [0 0 1];
 %% Formatting Building Data
 
 %%%Climate Zone
-cz_name = 'CZ08';
+cz_name = 'CZ06';
 %%%Year to be simulated
 yr = 2035;
 %%%Month filter - use during development/debugging
